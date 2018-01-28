d8271aec9a1df6c45d2218026b163fb28afc01a7
tmanzanares <tmanzanares@unm.edu>
2018-01-28 12:56:40 -0700

    Sentence objects have a list of indices of blurred out letters.

--- Game/Sentence.java
+++ Game/Sentence.java
@@ -1,21 +1,41 @@
+import java.util.ArrayList;
+
 class Sentence
 {
     public String original;
     public String scrambled;
+    public ArrayList<Integer> indices = new ArrayList<>();
 
     public Sentence(String original, String scrambled)
     {
         this.original = original;
         this.scrambled = scrambled;
+        addBlurredIndeces(scrambled);
     }
 
+    public void addBlurredIndeces(String scrambled)
+    {
+    	for(int i = 0; i < scrambled.length(); i++)
+    	{
+    		if(scrambled.charAt(i) == '?'){
+    			indices.add(i);
+    			System.out.println(i);
+    		}
+    	}
+    }
+    
     public String getOriginal()
     {
         return this.original;
     }
+    
     public String getScrambled()
     {
         return this.scrambled;
     }
 
+    public ArrayList<Integer> getIndices()
+    {
+    	return indices;
+    }
 }
