e2017963f874a2c712b91007ea3439c27e4ded24
CS-Labs <cseely@unm.edu>
2018-01-27 20:45:58 -0700

    update

--- Game/GameManager.java
+++ Game/GameManager.java
@@ -1,18 +1,24 @@
+import javax.xml.soap.Text;
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class GameManager
 {
     private int _level;
-    private ArrayList<String> _scrambledSentences = new ArrayList<String>();
-    private String _currentSentence;
-    Scrambler scrambler;
+    private Stack<Sentence> _levelSentences = new Stack<Sentence>();
+    private ArrayList<TextContainer> _levelTextContainers = new ArrayList<TextContainer>();
+    private TextContainer _currentSentence;
+    private Scramble scramble = new Scramble();
 
     GameManager()
     {
         _level = 1;
-       // _scrambledSentences = scrambler.getScrambledSentences();
-      //  _currentSentence =
+        _levelSentences = scramble.getter(_level);
+        _currentSentence = new TextContainer();
+        _currentSentence.setText( _levelSentences.pop().getScrambled(), 50,0,0);
     }
+
+
 
 
 
