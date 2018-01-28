37c600efc052f45e50b6bb82e37b5ca9bd0a0d2e
CS-Labs <cseely@unm.edu>
2018-01-27 19:34:25 -0700

    update

---
+++ Game/GameManager.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class GameManager
+{
+    private int _level;
+    private ArrayList<String> _scrambledSentences = new ArrayList<String>();
+    private String _currentSentence;
+    Scrambler scrambler;
+
+    GameManager()
+    {
+        _level = 1;
+       // _scrambledSentences = scrambler.getScrambledSentences();
+      //  _currentSentence =
+    }
+
+
+
+}
