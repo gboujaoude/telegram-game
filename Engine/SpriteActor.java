5e3fab433a6a2984c108a30b32293d7d4dc6d84f
KTStephano <amunra@unm.edu>
2018-01-28 13:22:06 -0700

    Adds sprite actor specifically for non-text images

---
+++ Engine/SpriteActor.java
@@ -0,0 +1,20 @@
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+/**
+ * This is a specialized actor which assumes you want
+ * to render with an image(s).
+ */
+public abstract class SpriteActor extends Actor {
+    private Image _texture;
+
+    public void setImage(String imageFile, int width, int height)
+    {
+        _texture = new Image(imageFile, width, height, false, true);
+    }
+
+    @Override
+    public void render(GraphicsContext gc) {
+        gc.drawImage(_texture, getX(), getY());
+    }
+}
